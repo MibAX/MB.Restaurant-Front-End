629c2f89ec2a3aa6257d2f77a99a8a9eb32903a6
MibAX Instructor <instructors@mibax.com>
2024-01-01 21:09:53 +0300

    Added ingredient delete confirmation dialog

--- src/app/ingredient/ingredient.component.ts
+++ src/app/ingredient/ingredient.component.ts
@@ -1,4 +1,5 @@
 import { ListService, PagedResultDto } from '@abp/ng.core';
+import { Confirmation, ConfirmationService, Toaster, ToasterService } from '@abp/ng.theme.shared';
 import { Component, OnInit } from '@angular/core';
 import { IngredientDto, IngredientService } from '@proxy/ingredients';
 
@@ -9,22 +10,50 @@ import { IngredientDto, IngredientService } from '@proxy/ingredients';
   providers: [ListService],
 })
 export class IngredientComponent implements OnInit {
-  
+
   ingredient = { items: [], totalCount: 0 } as PagedResultDto<IngredientDto>;
 
   constructor(
     public readonly list: ListService,
-    private ingredientSvc: IngredientService
-  ) {}
-  
+    private ingredientSvc: IngredientService,
+    private confirmation: ConfirmationService,
+    private toaster: ToasterService
+  ) { }
+
   ngOnInit(): void {
-    
+
     this.loadIngredients();
+  }
+
+  deleteIngredient(ingredient: IngredientDto): void {
+
+    const confirmOptions :  Partial<Confirmation.Options> = {
+      dismissible: false,
+      yesText: "Delete"
+    };
+    this.confirmation.error(`Are you sure you want to delete ${ingredient.name}?`, "Delete Ingredient", confirmOptions)
+      .subscribe((status: Confirmation.Status) => {
+
+        if (status === Confirmation.Status.confirm) {
+
+          this.ingredientSvc.delete(ingredient.id).subscribe({
+            next: () => {
+
+              this.list.get();
+              
+              const options: Partial<Toaster.ToastOptions> = {
+                messageLocalizationParams: [ingredient.name],
+              };
+              this.toaster.success('::DeletedSuccessfully', undefined, options);
+            }
+          });
+        }
+      });
   }
 
   //#region Private Functions
 
-  private loadIngredients() : void {
+  private loadIngredients(): void {
 
     const ingredientStreamCreator = (query) => this.ingredientSvc.getList(query);
 
--- src/app/shared/mat.module.ts
+++ src/app/shared/mat.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from "@angular/core";
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule,
-    MatDialogModule
+    MatButtonModule
   ],
   providers: []
 })
-export class MatModule {}
+export class MatModule { }
